4f97d4af16ddc9f413addfa041f92020781e7518
arjun391c <arjun391c@gmail.com>
2020-05-12 00:26:39 +0530

    dark2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,27 @@
 window.onload = () => {
-  const toggle = document.getElementById("dark");
+  let darkmode = localStorage.getItem("theme");
+
   const body = document.body;
+  const toggle = document.getElementById("dark");
+  if(darkmode === "enabled"){   
+    body.classList.add("dark");
+    toggle.checked = true;
+  }
+  else{
+    toggle.checked = false;
+    body.classList.remove("dark");
+  }
+ 
 
   toggle.addEventListener("input", (e) => {
     const isChecked = e.target.checked;
 
     if (isChecked) {
       body.classList.add("dark");
+      localStorage.setItem("theme","enabled")
     } else {
       body.classList.remove("dark");
+      localStorage.setItem("theme","disabled")
     }
   });
 };
