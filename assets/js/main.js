c65d9b346a40e68016dbe0c74dd05fd3915a19f0
arjun391c <arjun391c@gmail.com>
2020-05-11 23:50:38 +0530

    dark

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("light");
- }
+window.onload = () => {
+  const toggle = document.getElementById("dark");
+  const body = document.body;
+
+  toggle.addEventListener("input", (e) => {
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+      body.classList.add("dark");
+    } else {
+      body.classList.remove("dark");
+    }
+  });
+};
